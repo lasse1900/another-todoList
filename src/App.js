6e5a6a16fd22988ef33734cd998a3a3c21660894
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-18 12:49:55 +0200

    step 16 component to navigate to routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TodoForm, TodoList} from './components/todo'
-import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers'
-import {pipe, partial} from './lib/utils'
+import { TodoForm, TodoList, Footer } from './components/todo'
+import { addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo } from './lib/todoHelpers'
+import { pipe, partial } from './lib/utils'
 
 class App extends Component {
   state = {
     todos: [
-      {id: 1, name: 'Learn JSX', isComplete: true},
-      {id: 2, name: 'Build an Awesome App', isComplete: false},
-      {id: 3, name: 'Ship It', isComplete: false}
+      { id: 1, name: 'Learn JSX', isComplete: true },
+      { id: 2, name: 'Build an Awesome App', isComplete: false },
+      { id: 3, name: 'Ship It', isComplete: false }
     ],
     currentTodo: ''
   }
@@ -18,19 +18,19 @@ class App extends Component {
   handleRemove = (id, evt) => {
     evt.preventDefault()
     const updatedTodos = removeTodo(this.state.todos, id)
-    this.setState({todos: updatedTodos})
+    this.setState({ todos: updatedTodos })
   }
 
   handleToggle = (id) => {
     const getUpdatedTodos = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos))
     const updatedTodos = getUpdatedTodos(id, this.state.todos)
-    this.setState({todos: updatedTodos})
+    this.setState({ todos: updatedTodos })
   }
 
   handleSubmit = (evt) => {
     evt.preventDefault()
     const newId = generateId()
-    const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
+    const newTodo = { id: newId, name: this.state.currentTodo, isComplete: false }
     const updatedTodos = addTodo(this.state.todos, newTodo)
     this.setState({
       todos: updatedTodos,
@@ -64,10 +64,11 @@ class App extends Component {
           {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
           <TodoForm handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
-            handleSubmit={submitHandler}/>
+            handleSubmit={submitHandler} />
           <TodoList handleToggle={this.handleToggle}
             todos={this.state.todos}
             handleRemove={this.handleRemove} />
+          <Footer />
         </div>
       </div>
     );
